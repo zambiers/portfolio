ac92e78c55c69a597860db435db5decd778f7609
Avah <114538590+zambiers@users.noreply.github.com>
2025-06-23 16:18:16 -0700

    Main page started

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,32 +1,78 @@
 
 import React, { useState } from 'react';
 import Navbar from '../assets/components/NavBar';
-import headShot from '../assets/images/headShot.png';
+import headshot from '../assets/images/headshot.jpg';
+import github from '../assets/images/links/github.png';
+import linked from '../assets/images/links/linked.png';
+import art from '../assets/images/links/art.png';
+import itch from '../assets/images/links/itch.png';
+import gmail from '../assets/images/links/gmail.jpg';
 import '../App.css'; 
 
 function Home() {
-  return (<>
-  <div className='home-page-image'>
-      <img src = {headShot}  className= "headShot" />
-   </div>
-  <h1>
-    My name is Avah Afshari, and this is my website.
-  </h1>
-  <h2>
-    A little about me: 
-  </h2>
-  <h3>
-    I made this website to keep all my projects all in one place without having to give multiple different
-    links. 
-  </h3>
-  
+  return (
+    <div className="home-wrapper">
+      <div className="home-page-image">
+        <img src={headshot} alt="Avah Afshari" className="headShot" />
+      </div>
+      
+      <div className="home-text">
+        <h1>My name is</h1>
+        <h1 className='Name'> Avah Afshari.</h1>
+        <h1>Welcome to my portfolio!</h1>
+      </div>
 
-  <div>
-    <h1>
-      My most recent projects
-    </h1>
-  </div>
-  </>);
-}
+      <h2>
+          I made this website to keep all my projects all in one place without
+          having to give multiple different links. Feel free to explore!
+      </h2>
+
+      <hr className='divider' />
+
+    <h1 className='lower-home'>My Most Recent Project</h1>
+    
+
+
+
+
+      <hr className='divider' />
+      <h1 className='lower-home'>Project Links</h1>
+      <div className='socials'>
+      <a
+          href="https://github.com/zambiers" target="_blank" rel="noopener noreferrer" className="circle-link">
+          <img src={github} alt="GitHub" />
+      </a>
+
+       <a
+          href="https://www.linkedin.com/in/avah-afshari/" target="_blank" rel="noopener noreferrer" className="circle-link">
+          <img src={linked} alt="LinkedIn" />
+      </a>
+
+      <a
+          href="https://www.artstation.com/avahafshari2" target="_blank" rel="noopener noreferrer" className="circle-link">
+          <img src={art} alt="ArtStation" />
+      </a>
+
+      <a
+          href="https://zambiers.itch.io/" target="_blank" rel="noopener noreferrer" className="circle-link">
+          <img src={itch} alt="Itch.io" />
+      </a>
+      </div>
+
+      <div>
+        <h1 className='lower-home'>Contact</h1>
+          <div className='socials'>
+          <h2> aafshari227@gmail.com or </h2>
+            <a
+              href="https://www.linkedin.com/in/avah-afshari/" target="_blank" rel="noopener noreferrer" className="circle-link">
+              <img src={linked} alt="LinkedIn" />
+            </a>
+
+          </div>
+      </div>
+    
+  </div>    
+  );
+};
 
 export default Home;
