6bc477cc6b65d9b0d3950094f73c48f3ce203424
Avah <114538590+zambiers@users.noreply.github.com>
2025-06-26 12:04:45 -0700

    started working on the project pages

--- src/pages/ubuntu.js
+++ src/pages/ubuntu.js
@@ -1,10 +1,37 @@
 import React from "react";
 import Navbar from '../assets/components/NavBar';
-import '../App.css'; 
+import '../App.css';
+import '../Styling/projects.css';
 
 
 function Ubuntu(){
-    return(<>This would be the page for finding ubuntu</>);
+    return(<>
+        <h1>Finding Ubuntu</h1>
+        <h2>Unlikely Collaborators Game Jam 2025</h2>
+        <h1 className='links-section'></h1>
+                  <div className="link-group">
+                    <h3>
+                        <a 
+                        href="https://github.com/zambiers/UC_Scavryzaro/tree/main/UC_Scavryzaro" 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        className="links"
+                        >
+                        Github
+                        </a>
+                    </h3>
+                    <h3>
+                        <a 
+                        href="https://roshellepixelle.itch.io/finding-ubuntu" 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        className="links"
+                        >
+                        Itch.io 
+                        </a>
+                    </h3>
+                    </div>       
+    </>);
 }
 
 export default Ubuntu;
