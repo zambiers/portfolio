f83eddca4837ac1b6fe528c15906ba5a617251bb
Avah <assassinkenway207@gmail.com>
2026-07-29 15:03:33 -0700

    quick fix

--- src/assets/components/NavBar.js
+++ src/assets/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from "react-router-dom";
+import { useState } from 'react-router-dom';
 import './NavBarStyles.css'; 
 
 function Navbar() {
